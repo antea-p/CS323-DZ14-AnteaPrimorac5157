7c4a4dbb68b15ade5197c514b1c675bcce049adb
Antea P <antea.primorac0@gmail.com>
2024-07-24 22:10:50 +0200

    updated AppHiltModule

--- app/src/main/java/rs/ac/metropolitan/cs330_dz14_anteaprimorac5157/di/AppHiltModule.kt
+++ app/src/main/java/rs/ac/metropolitan/cs330_dz14_anteaprimorac5157/di/AppHiltModule.kt
@@ -2,10 +2,16 @@ package rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.di
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.network.ApiService
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.network.RetrofitHelper
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.network.RetrofitHelperImpl
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.repository.Repository
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.repository.RepositoryImpl
 import javax.inject.Singleton
 
 
@@ -17,6 +23,26 @@ object AppHiltModule {
     fun provideContext(application: Application): Context {
         return application.applicationContext
 
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitHelper(): RetrofitHelper {
+        Log.d("AppHiltModule", "Providing RetrofitHelper")
+        return RetrofitHelperImpl()
+    }
+    @Provides
+    @Singleton
+    fun provideApiService(retrofitHelper: RetrofitHelper): ApiService {
+        Log.d("AppHiltModule", "Providing ApiService")
+        return retrofitHelper.getApiService()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(apiService: ApiService): Repository {
+        Log.d("AppHiltModule", "Providing Repository")
+        return RepositoryImpl(apiService)
     }
 
 }
