ad4ddbb43744914e739b8dbf89584e81bb95656a
Antea P <antea.primorac0@gmail.com>
2024-07-26 17:53:27 +0200

    created AppViewModelImpl, without HiltViewModel for now

---
+++ app/src/main/java/rs/ac/metropolitan/cs330_dz14_anteaprimorac5157/ui/viewmodel/AppViewModelImpl.kt
@@ -0,0 +1,49 @@
+package rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.model.Company
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.data.model.CompanyType
+import rs.ac.metropolitan.cs330_dz14_anteaprimorac5157.domain.UseCases
+import javax.inject.Inject
+
+// TODO:
+//@HiltViewModel
+class AppViewModelImpl @Inject constructor(
+    private val useCases: UseCases
+) : ViewModel(), AppViewModel {
+    private var currentType: CompanyType? = null
+    private var _companies = MutableLiveData<List<Company>>(emptyList())
+    private var _isLoading = MutableLiveData<Boolean>(false)
+    private var _internetPermissionGranted = MutableLiveData<Boolean>(false)
+
+    override val companies: LiveData<List<Company>>
+        get() = _companies
+    override val isLoading: LiveData<Boolean>
+        get() = _isLoading
+    override val internetPermissionGranted: LiveData<Boolean>
+        get() = _internetPermissionGranted
+
+    override fun loadCompanies() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val companiesFlow = currentType?.let { useCases.getCompaniesByType(it) } ?: useCases.getCompanies()
+            companiesFlow.collect { companies ->
+                _companies.value = companies
+                _isLoading.value = false
+            }
+        }
+    }
+
+    override fun setTabCompanyType(companyType: CompanyType) {
+        currentType = companyType
+        loadCompanies()
+    }
+
+    override fun setInternetPermissionGranted(granted: Boolean) {
+        _internetPermissionGranted.value = granted
+    }
+}
